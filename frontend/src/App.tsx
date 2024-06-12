0c00360613365630cd6c12ea6f7bae4741cd45d3
kal2 <kanealong@gmail.com>
2024-06-12 15:12:13 -0500

    v0.02 - Frontend Wireframe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,21 +16,23 @@ function App() {
     <div id="app">
       <div id="data-table">
         <Table>
-          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableCaption>A list of open/pending recomendations.</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="w-[100px]">Name</TableHead>
+              <TableHead>Company</TableHead>
+              <TableHead>Sample ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Date Complete</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             <TableRow>
-              <TableCell className="font-medium">INV001</TableCell>
-              <TableCell>Paid</TableCell>
-              <TableCell>Credit Card</TableCell>
-              <TableCell className="text-right">$250.00</TableCell>
+              <TableCell className="font-medium">Kyle</TableCell>
+              <TableCell>Kinsey Family Vineyard</TableCell>
+              <TableCell>0401202</TableCell>
+              <TableCell>03/22/2023</TableCell>
+              <TableCell className="text-right">01/04/2024</TableCell>
             </TableRow>
           </TableBody>
         </Table>
